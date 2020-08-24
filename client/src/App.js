24ef292560a97513ca9f5aef4c2277f7b831f931
Uzor <p.uzor99@gmail.com>
2020-08-24 14:09:15 +0100

    Added client side
    Installed react and have setup the basic configurations

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from "./components/Layout/Navbar";
+import './App.css';
+
+
+const App = () => {
+    return (
+        <div className="App">
+            <Navbar/>
+            My App
+        </div>
+    );
+}
+
+export default App;
