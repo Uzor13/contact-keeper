7c5d05c3e71f6cdb4588ff4217a00ad64c763dbc
Uzor <p.uzor99@gmail.com>
2020-08-24 13:24:50 +0100

    Server
    Added the server routes for contact, auth and user. Configures the db using mongo

---
+++ routes/contacts.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const {check, validationResult} = require('express-validator');
+const auth = require('../middleware/auth');
+const router = express.Router();
+
+const User = require('./models/User');
+const Contact = require('./models/Contact')
+
+
+// @route GET api/contacts
+// @desc Get all users contact
+// @access Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const contacts = await Contact.find({user: req.user.id}).sort({date: -1});
+        res.json(contacts);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
+// @route POST api/contacts
+// @desc Add new contact
+// @access Private
+router.post('/', [auth, [
+        check('name', 'Name is required')
+            .not()
+            .isEmpty()
+    ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()});
+        }
+        const {name, email, phone, type} = req.body;
+        try {
+            const newContact = new Contact({
+                name,
+                email,
+                phone,
+                type,
+                user: req.user.id
+            });
+
+            const contact = await newContact.save();
+            res.json(contact);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    });
+
+// @route PUT api/contacts/:id
+// @desc Update contact
+// @access Private
+router.put('/:id', (req, res) => {
+    res.send('Update Contact');
+});
+
+// @route DELETE api/contacts/:id
+// @desc Delete contact
+// @access Private
+router.delete('/:id', (req, res) => {
+    res.send('Delete Contact');
+});
+
+module.exports = router;
